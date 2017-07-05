394b75d37b61704f4030e0c00cedbdaf4ec89aef
EduardoSantosGit <eduardosantos058@gmail.com>
2017-07-05 20:06:35 -0300

    method logic prime init tests

--- sieve/Sieve.cs
+++ sieve/Sieve.cs
@@ -1,9 +1,21 @@
 using System;
+using System.Collections.Generic;
 
 public static class Sieve
 {
     public static int[] Primes(int limit)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        List<int> lista = new List<int>();
+
+        for(int i = 2;i<=limit;i++){
+            lista.Add(i);
+        }
+
+        for(int i=1;i<lista.Count;i++){
+            if(lista[i] % i == 0){
+                lista.Remove(i);
+            }
+        }
+        return lista.ToArray();
     }
 }
