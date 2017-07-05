e76109d1611d1a305e626c223d0caf807913cd49
EduardoSantosGit <eduardosantos058@gmail.com>
2017-07-05 20:55:51 -0300

    code refactor logic algorithm

--- sieve/Sieve.cs
+++ sieve/Sieve.cs
@@ -5,17 +5,27 @@ public static class Sieve
 {
     public static int[] Primes(int limit)
     {
-        List<int> lista = new List<int>();
+        List<bool> primos = new List<bool>();
+        List<int> result = new List<int>();
 
-        for(int i = 2;i<=limit;i++){
-            lista.Add(i);
+        for(int i=0;i<=limit;i++){
+            primos.Add(true);
         }
-
-        for(int i=1;i<lista.Count;i++){
-            if(lista[i] % i == 0){
-                lista.Remove(i);
+         
+        for(int p = 2; p*p <=limit; p++)
+        {
+            if(primos[p] == true)
+            {
+                for(int i = p*2; i <= limit; i += p)
+                    primos.Add(false);
             }
         }
-        return lista.ToArray();
+        for(int i = 2; i <= limit; i++)
+        {
+            if(primos[i] == true)
+               result.Add(i);
+        }
+
+        return result.ToArray();
     }
 }
