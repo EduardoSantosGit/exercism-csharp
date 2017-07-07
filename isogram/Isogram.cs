b9e5808194738f6d45b6c795f2446ba545c4de51
EduardoSantosGit <eduardosantos058@gmail.com>
2017-07-07 11:21:54 -0300

    [isogram] start exercise

---
+++ isogram/Isogram.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public static class Isogram
+{   
+    public static bool IsIsogram(string word)
+    {   
+        if(word.Length == 0) return true;
+        
+        int contador = 0;
+        word = word.Replace("-","").Replace(" ","");
+        for(int i=0;i<word.Length;i++){
+            for(int j=0;j<word.Length;j++){
+                if(word[i] == word[j]){
+                    contador += 1;
+                }
+            }
+        }
+        if(contador > 1){
+            return true;
+        }
+        return false;
+    }
+}
