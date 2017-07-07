14257f185390a247a2601105992f2d6bad5267fa
EduardoSantosGit <eduardosantos058@gmail.com>
2017-07-07 12:45:28 -0300

    [secret-handshake] implements include logic calc binary and rule sequence array init tests

--- secret-handshake/SecretHandshake.cs
+++ secret-handshake/SecretHandshake.cs
@@ -1,10 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 public static class SecretHandshake
 {
+    private static readonly Dictionary<int, string> _secrets = new Dictionary<int, string>()
+                   {{1,"wink"}, {10,"double blink"},{100,"close your eyes"}, {1000,"jump"}};
+    private static List<string> _shakes = new List<string>();               
+    private static string _numberBinary;
     public static string[] Commands(int commandValue)
-    {
-        throw new NotImplementedException("You need to implement this function.");
+    {   
+        while (commandValue > 0)
+        {
+            var _rest = (commandValue % 2);
+            _numberBinary = _numberBinary + _rest.ToString();
+            commandValue = commandValue / 2;
+        }
+        _numberBinary = new string(_numberBinary.ToCharArray().Reverse().ToArray());
+        foreach(var item in _secrets){
+            if(Convert.ToInt32(_numberBinary) >= item.Key){
+                _shakes.Add(item.Value);
+            }
+        }
+        return _shakes.ToArray();
     }
 }
