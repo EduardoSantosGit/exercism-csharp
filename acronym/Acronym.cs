44f876e341b85cd0a5fba162a34fe7941e621b4e
EduardoSantosGit <eduardosantos058@gmail.com>
2017-07-10 17:24:54 -0300

    [acronym] - implements logic ling select first letter word init tests

--- acronym/Acronym.cs
+++ acronym/Acronym.cs
@@ -1,9 +1,14 @@
 ﻿using System;
+using System.Linq;
 
 public static class Acronym
 {
     public static string Abbreviate(string phrase)
     {
-        throw new NotImplementedException("Please implement this function");
+        phrase = phrase.Replace("-"," ").Replace(":","");
+        return new string(phrase
+                     .Split(' ')
+                     .Select(x => x.First())
+                     .ToArray());
     }
 }
