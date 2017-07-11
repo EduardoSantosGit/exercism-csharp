0529c8bd61739b0dc3acc3d131f572524094d166
EduardoSantosGit <eduardosantos058@gmail.com>
2017-07-11 11:38:01 -0300

    [scrabble-score] - refactor code complete tests success use linq

--- scrabble-score/ScrabbleScore.cs
+++ scrabble-score/ScrabbleScore.cs
@@ -1,34 +1,23 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 public static class Scrabble
 {  
     public static int Score(string input)
-    {
-        Dictionary<int, List<char>> hashMap = new Dictionary<int, List<char>>();
-        hashMap.Add(1, new List<char> {'A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'});
-        hashMap.Add(2, new List<char> {'D','G'});
-        hashMap.Add(3, new List<char> {'B', 'C', 'M', 'P'});
-        hashMap.Add(4, new List<char> {'F', 'H', 'V', 'W', 'Y'});
-        hashMap.Add(5, new List<char> {'K'});
-        hashMap.Add(8, new List<char> {'J','X'});
-        hashMap.Add(10, new List<char> {'Q','Z'});
-
-        int contador = 0;
-        if(input == null || input == ""){
-            return contador;
-        }
-
-        input = input.ToUpper();
-        for(int i=0;i<input.Length;i++){
-           foreach(var item in hashMap){
-               foreach(var letras in item.Value){
-                   if(input[i] == letras){
-                      contador += item.Key;
-                   }
-               }
-           }
-        }
-
-        return contador;
+    {   
+        //refactor code
+        if(input == null || input == "") return 0;
+        
+        Dictionary<int,List<char>> hashmap = new Dictionary<int, List<char>>()
+        {
+            {1, new List<char> {'A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'}},
+            {2, new List<char> {'D','G'}},
+            {3, new List<char> {'B', 'C', 'M', 'P'}},
+            {4, new List<char> {'F', 'H', 'V', 'W', 'Y'}},
+            {5, new List<char> {'K'}},
+            {8, new List<char> {'J','X'}},
+            {10, new List<char> {'Q','Z'}}
+        };
+        return hashmap.Sum(key => key.Key * input.ToUpper().Count(value => key.Value.Contains(value)));
     }
 }
