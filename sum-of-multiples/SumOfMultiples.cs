0e190acdace2878e14118e77da9397520a751ab0
EduardoSantosGit <eduardosantos058@gmail.com>
2017-07-11 19:10:19 -0300

    [sum-of-multiples] refactor code change names variables

--- sum-of-multiples/SumOfMultiples.cs
+++ sum-of-multiples/SumOfMultiples.cs
@@ -5,10 +5,9 @@ public static class SumOfMultiples
 {
     public static int To(IEnumerable<int> multiples, int max)
     {   
-        int resposta = 0;
+        var result = 0;
         List<int> numList = new List<int>();
-        foreach (int num in multiples)
-        {
+        foreach (int num in multiples){
             for (int i = 0; i < max; i++)
             {
                 if (i % num == 0 && !numList.Contains(i))
@@ -16,9 +15,8 @@ public static class SumOfMultiples
             }
         }
         for(int i=0;i<numList.Count;i++){
-            resposta += numList[i];
+            result += numList[i];
         }
-
-        return resposta;
+        return result;
     }
 }
