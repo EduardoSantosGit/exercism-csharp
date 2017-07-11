ad50259c54c9dbbe38765521932361361cd90f77
EduardoSantosGit <eduardosantos058@gmail.com>
2017-07-11 15:35:46 -0300

    [etl] - refactor code using linq replacing foreach

--- etl/Etl.cs
+++ etl/Etl.cs
@@ -1,31 +1,24 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public static class ETL
 {
     public static IDictionary<string, int> Transform(IDictionary<int, IList<string>> old)
-    {   
+    {
+
         //recode
-        Dictionary<string,int> hashResult = new Dictionary<string,int>();
+        // Dictionary<string, int> hashResult = new Dictionary<string, int>();
 
-        foreach (var key in old){
-             foreach(var res in key.Value){
-                hashResult.Add(res.ToLower(), key.Key);
-             }    
-        }
-        return hashResult;
-        
-        //antigo
-        /*Dictionary<string,int> hashResult = new Dictionary<string,int>();
-        List<string> letras = new List<string>();
-        foreach (var key in old){
-            letras.AddRange(key.Value);
-            foreach(var res in letras){
-                hashResult.Add(res.ToLower(), key.Key);
-            }
-            letras.Clear();
-        }
-        return hashResult;*/
+        // foreach (var key in old)
+        // {
+        //     foreach (var res in key.Value)
+        //     {
+        //         hashResult.Add(res.ToLower(), key.Key);
+        //     }
+        // }
 
+        return old.SelectMany(e => e.Value.Select(s => new { k = s.ToLowerInvariant(), v = e.Key }))
+                  .ToDictionary(x => x.k, x => x.v);
     }
 }
