11b3566c1845d4371dda36daef1e4733f27c967a
EduardoSantosGit <eduardosantos058@gmail.com>
2017-07-11 09:39:37 -0300

    [matrix] refactor code method row

--- matrix/Matrix.cs
+++ matrix/Matrix.cs
@@ -1,16 +1,28 @@
 ﻿using System;
+using System.Linq;
 
 public class Matrix
-{
+{   
+    private string _input;
+    private string[] _arrayCube;
+    private int[] _rows;
+    private int _row;
     public Matrix(string input)
     {
+        _input = input;
     }
 
     public int Rows
     {
         get
-        {
-            throw new NotImplementedException("You need to implement this function.");
+        {   
+            var contador = 0;
+            for(int i=0;i<_input.Length;i++){
+                if(_input[i] == '\''){
+                    contador += 1;
+                }
+            }
+            return contador +1;   
         }
     }
 
@@ -23,9 +35,17 @@ public class Matrix
     }
 
     public int[] Row(int row) 
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+    {  
+        _row = row;
+        _arrayCube = _input.Split('\n');
+        for(int i=0;i<_arrayCube.Length;i++){
+            if(Int32.Parse(_arrayCube[i]) == row){
+                _rows = new int [] {Int32.Parse(_arrayCube[i])};
+            }
+        }
+        Console.WriteLine(_rows);
+        return _rows;
+    }   
 
     public int[] Col(int col)
     {
