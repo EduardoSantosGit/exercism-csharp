ca7ffe3aedd5cf633bd9788c8ed90ea83f5c2ea3
EduardoSantosGit <eduardosantos058@gmail.com>
2017-07-12 15:56:11 -0300

    [nucleotide-count] refactor code implements linq and remove foreach and change variables names

--- nucleotide-count/NucleotideCount.cs
+++ nucleotide-count/NucleotideCount.cs
@@ -1,12 +1,13 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public class DNA
-{   
-    private string sequencia;
+{
+    private string _sequence;
     public DNA(string sequence)
     {
-        sequencia = sequence;
+        _sequence = sequence;
     }
 
     public IDictionary<char, int> NucleotideCounts
@@ -24,19 +25,27 @@ public class DNA
 
     public int Count(char nucleotide)
     {
+        if (_sequence.Where(x => x == nucleotide).Count() == 0 
+             && !nucleotide.Equals('A') && !nucleotide.Equals('T')
+             && !nucleotide.Equals('C') && !nucleotide.Equals('G'))
+            throw new InvalidNucleotideException();
+
+        return _sequence.Where(x => x == nucleotide).Count();
+
+        /* 
         int resposta = 0;
         for(int i=0;i<sequencia.Length;i++){
             if(sequencia[i].Equals(nucleotide)){
                 resposta += 1;
             }
-        }
-        if(resposta == 0){
+        }*/
+        /*if(answer == 0){
             if(!nucleotide.Equals('A') && !nucleotide.Equals('T')
             && !nucleotide.Equals('C') && !nucleotide.Equals('G')){
                 throw new InvalidNucleotideException();
             } 
         }
-        return resposta;
+        return answer;*/
     }
 }
 
