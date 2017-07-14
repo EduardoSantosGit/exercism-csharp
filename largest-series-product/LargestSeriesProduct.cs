64695f9fdca9775d06821e5b1c82f49e8d04c51a
EduardoSantosGit <eduardosantos058@gmail.com>
2017-07-14 16:31:00 -0300

    [largest-series-product] implements method numbers linq fix length array

--- largest-series-product/LargestSeriesProduct.cs
+++ largest-series-product/LargestSeriesProduct.cs
@@ -1,9 +1,14 @@
 using System;
+using System.Linq;
 
-public static class LargestSeriesProduct
-{
+public static class LargestSeriesProduct{
     public static long GetLargestProduct(string digits, int span) 
-    {
-        throw new NotImplementedException("You need to implement this function.");
+    {   
+        if(span > digits.Length) throw new ArgumentException();
+        
+        return Enumerable.Range(0,digits.Length - span + 1)
+                            .Max(x => digits.Substring(x ,span))
+                            .Select(x => (int)(x - '0'))
+                            .Aggregate(1, (a,b) => a * b);                 
     }
 }
